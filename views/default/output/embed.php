89dc1a2d291e8b6064439ac0db2766cfad2850c3
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-07-19 10:02:23 +0200

    Remove redundancies

--- views/default/output/embed.php
+++ views/default/output/embed.php
@@ -4,15 +4,6 @@ namespace hypeJunction\Embed;
 
 $url = elgg_extract('value', $vars);
 
-// If the URL qualifies an existing entity, just use the entity view
-$guid = get_guid_from_url($url);
-if ($guid && ($entity = get_entity($guid))) {
-	echo elgg_view('embed/item/entity', array(
-		'entity' => $entity
-	));
-	return;
-}
-
 echo elgg_view('embed/item/url', array(
 	'url' => $url
 ));

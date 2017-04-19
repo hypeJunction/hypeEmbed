ad4f38409468da5b72eba773b60013d47748247c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-04-19 15:05:17 +0200

    fix(privacy): only allow public-facing entities to be embedded

--- views/default/embed/entity/default.php
+++ views/default/embed/entity/default.php
@@ -2,6 +2,14 @@
 
 $entity = elgg_extract('entity', $vars);
 
+$type = $entity->getType();
+$subtype = $entity->getSubtype();
+
+$subtypes = get_registered_entity_types($type);
+if ($subtype && !in_array($subtype, $subtypes)) {
+	return;
+}
+
 echo elgg_view_entity($entity, [
 	'full_view' => false,
 	'size' => elgg_extract('size', $vars),
